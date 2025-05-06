72d12ca33d4b51e7dde4b774668bca795f078909
wladij <joew.candelario@gmail.com>
2025-05-06 00:54:30 -0500

    index page commit

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+console.log('JS cargado');
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const currentYear = new Date().getFullYear();
+    document.getElementById("current-year").innerText = currentYear;
+
+    
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").innerText = "Last Modified: " + lastModified;
+});
